917d6d38e6db8d0192b63d70d40d630da7aa2bb1
Amal Nath M <amalnathm7@gmail.com>
2023-08-01 18:26:02 +0530

    Typescript introduced

--- app.ts
+++ app.ts
@@ -1,15 +1,27 @@
 // const http = require('http');
-const express = require('express');
+import express from 'express';
 
 const server = express();
 
 server.get('/hello', (req, res) => {
-    console.log(req.url);
+    // console.log(req.url);
+    // const data = "amal";
+
+    // const data1 = {
+    //     name: "name1",
+    //     profile: {
+    //         age: 20
+    //     }
+    // };
+
+    // // console.log(data.profile);
+    // console.log(data1.profile.age);
+
     res.status(200).send("Hello World!");
 });
 
 server.get('/*', (req, res) => {
-    console.log(req.url);
+    // console.log(req.url);
     res.status(200).send("404 bro!");
 });
 
