da6eeb3e951642b9a0d6ee2e4aca2859f4f0e491
Amal Nath M <amalnathm7@gmail.com>
2023-08-01 15:37:52 +0530

    Created Express server

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-const http = require('http');
+// const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
+const server = express();
+
+server.get('/hello', (req, res) => {
     console.log(req.url);
-    res.writeHead(200);
-    res.end("Hello World!");
+    res.status(200).send("Hello World!");
 });
+
+server.get('/*', (req, res) => {
+    console.log(req.url);
+    res.status(200).send("404 bro!");
+});
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
+//     res.writeHead(200);
+//     res.end("Hello World!");
+// });
 
 server.listen(3000, () => {
     console.log("Server is listening to 3000");
