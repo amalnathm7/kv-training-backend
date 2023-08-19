ae124cb83eecd96e77cb790f2171ebfcf8206484
alanphiliposenv <alanphiliposenv.mec@gmail.com>
2023-08-19 10:45:55 +0530

    referral dto

---
+++ src/dto/create-referral.dto.ts
@@ -0,0 +1,61 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsObject,
+  IsString,
+  ValidateIf,
+  ValidateNested,
+} from "class-validator";
+import Address from "../entity/address.entity";
+import { Type } from "class-transformer";
+import CreateAddressDto from "./create-address.dto";
+import { Status } from "../utils/status.enum";
+import { ValidateDto } from "./validate.dto";
+
+class CreateReferralDto implements ValidateDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  experience: number;
+
+  @IsNotEmpty()
+  @IsString()
+  phone: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @IsNotEmpty()
+  @IsString()
+  resume: string;
+
+  @IsNotEmpty()
+  @IsString()
+  referredBy: string;
+
+  @IsNotEmpty()
+  @IsObject()
+  @ValidateNested({ each: true })
+  @Type(() => CreateAddressDto)
+  address: Address;
+
+  @IsNotEmpty()
+  @IsString()
+  roleId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  opening: string;
+}
+
+export default CreateReferralDto;
