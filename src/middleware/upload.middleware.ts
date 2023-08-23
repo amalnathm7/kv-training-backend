7c5259d2187cf71eb54c5c2d1e790231857b289c
Vidya V <vidyav10901@gmail.com>
2023-08-23 21:41:47 +0530

    file download

--- src/middleware/upload.middleware.ts
+++ src/middleware/upload.middleware.ts
@@ -1,31 +1,19 @@
 import util from 'util';
 import multer from 'multer';
-import filesystem from 'fs';
-
-const folderPath = './uploads';
+import filesystem from 'fs';const folderPath = './uploads';
 const key = 'file';
-const maxSize = 5 * 1024 * 1024;
-
-let storage = multer.diskStorage({
+const maxSize = 5 * 1024 * 1024;let storage = multer.diskStorage({
     destination: (req, file, callback) => {
         if (!filesystem.existsSync(folderPath)) {
             filesystem.mkdirSync(folderPath);
-        }
-
-        callback(null, folderPath);
+        }        callback(null, folderPath);
     },
     filename: (req, file, callback) => {
         const filePath = Date.now() + '_' + file.originalname;
         callback(null, filePath);
-        req.filePath = folderPath + '/' + filePath;
+        req.filePath = filePath;
     },
-});
-
-let uploadFile = multer({
+});let uploadFile = multer({
     storage: storage,
     limits: { fileSize: maxSize },
-}).single(key);
-
-let uploadFileMiddleware = util.promisify(uploadFile);
-
-export default uploadFileMiddleware;
+}).single(key);let uploadFileMiddleware = util.promisify(uploadFile);export default uploadFileMiddleware;
